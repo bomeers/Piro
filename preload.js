7ec54507973b95e73b2a713c55d417674828831d
Bo Meers <31780831+bomeers@users.noreply.github.com>
2019-09-06 00:56:16 -0500

    fixed version

--- preload.js
+++ preload.js
@@ -1,6 +1,5 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-var appVersion = require('electron').remote.app.getVersion();
 window.addEventListener('DOMContentLoaded', () => {
 
   // Master Page - Header
