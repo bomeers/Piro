72958c716a24acff763ef88ab90c6ce0dd1ff551
Bo Meers <31780831+bomeers@users.noreply.github.com>
2019-09-07 02:59:43 -0500

    fixed a buncha stuff

---
+++ app/Main/preload.js
@@ -0,0 +1,22 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  var appVersion = require('electron').remote.app.getVersion();
+  document.getElementById('version').innerText = appVersion;
+
+  const {
+    ipcRenderer
+  } = require('electron');
+  ipcRenderer.on('message', function (event, text) {
+    var container = document.getElementById('messages');
+    var message = document.createElement('div');
+    message.innerHTML = text;
+    container.appendChild(message);
+  })
+
+  // Master Page - Header
+  const loadJQuery = document.createElement("script");
+  loadJQuery.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+  loadJQuery.type = 'text/javascript';
+  document.body.appendChild(loadJQuery);
+})
